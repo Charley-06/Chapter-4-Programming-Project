77a1917c09a0b4864101c688a128fdce57315a57
Charley <bobosoepic@gmail.com>
2024-10-02 03:06:54 +0000

    wrote main code

--- .ccls-cache/@home@runner@Chapter-2-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Programming-Project/main.cpp
@@ -2,4 +2,8 @@
 
 int main() {
 // This program will calculate this differences in distance a car will drive through town and on the highway.
-  
+const double gallon_tank = 20;
+double town_miles = gallon_tank * 23.5;
+double highway_miles = gallon_tank * 28.9;
+std::cout << "When driven in town, the car will travel " << town_miles << " miles.\n";
+std::cout << "When driven on the highway, the car will travel " << highway_miles << " miles.\n";}
