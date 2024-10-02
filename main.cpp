c88c6c70dae1e350bfa50bb0f8f988d81673a535
Charley <bobosoepic@gmail.com>
2024-10-02 02:55:34 +0000

    made comment

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 
 int main() {
-// this program will show the rate of ocean level increase at 5, 7, and 10 years.
-const double rise_per_year = 1.5;
-double rise_in_5_years = rise_per_year * 5;
-double rise_in_7_years = rise_per_year * 7;
-double rise_in_10_years = rise_per_year * 10;
-std::cout << "The ocean's level will be " << rise_in_5_years << " millimeters higher in 5 years.\n";
-std::cout << "The ocean's level will be " << rise_in_7_years << " millimeters higher in 7 years.\n";
-std::cout << "The ocean's level will be " << rise_in_10_years << " millimeters higher in 10 years.\n";}
+// This program will calculate this differences in distance a car will drive through town and on the highway.
+  
