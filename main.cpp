00e0dd15c69f75b3ed43fb077481e01d472de3ea
Charley <bobosoepic@gmail.com>
2024-10-11 01:56:57 +0000

    Wrote code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//This code will ask for the principle amout, interest rate, and the number of times the interest is compounded. It will then give the amount of interest earned.
 
 int main() {
-// This program will calculate out of people surveyed, how many drank energy drinks and how many out of those preffered citrus flavored ones.
-const double customers = 16500;
-double eg_consumers = customers * 0.15;
-double citrus_enjoyers = eg_consumers * 0.58;
-std::cout << "Out of people surveyed, " << eg_consumers << " said they drank one or more energy drinks per week.\n";
-std::cout << "Out of the people who drank energy drinks, " << citrus_enjoyers << " said they preferred citrus flavored energy drinks.\n";}
+double principle;
+double interest;
+double compounded;
+double insavings;
+
+cout << "Enter the principle amount: ";
+cin >> principle;
+cout << "Enter the interest rate: ";
+cin >> interest;
+cout << "Enter the number of times compounded: ";
+cin >> compounded;
+
+insavings = principle * pow((1 + interest / compounded), compounded);
+
+cout << "Amount in savings: $" << insavings << endl;
+}
