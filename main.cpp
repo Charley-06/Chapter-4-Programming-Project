b5e8bd44cdb638ecd3bcbd5f389eb9f812f6aade
Charley <bobosoepic@gmail.com>
2024-10-11 01:27:28 +0000

    Wrote code and added comment

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-// This program will calculate this differences in distance a car will drive through town and on the highway.
-const double gallon_tank = 20;
-double town_miles = gallon_tank * 23.5;
-double highway_miles = gallon_tank * 28.9;
-std::cout << "When driven in town, the car will travel " << town_miles << " miles.\n";
-std::cout << "When driven on the highway, the car will travel " << highway_miles << " miles.\n";}
+srand(time(0));
+
+int randomNumber = rand() % 100 + 1;
+int randomNumber2 = rand() % 100 + 1;
+cout << "What is " << randomNumber << " + " << randomNumber2 << "?" << endl;
+cout << "Press enter to see the answer" << endl;
+cin.get();
+
+int correctAnswer = randomNumber + randomNumber2;
+cout << "The answer is: " << correctAnswer << endl;
+}
