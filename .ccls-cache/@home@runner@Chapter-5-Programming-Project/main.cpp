bfb9b40ffde6ec6cc5dac14bc645d4ec2785c404
Charley <bobosoepic@gmail.com>
2024-12-14 05:03:50 +0000

    wrote code

--- .ccls-cache/@home@runner@Chapter-5-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5-Programming-Project/main.cpp
@@ -1,22 +1,71 @@
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
-// Uses a chart to covert the temperature from Fahrenheit to Celsius using a formula
+int validateRooms() {
+int numRooms;
+do {
+cout << "Enter the number of rooms: ";
+cin >> numRooms;
+if (numRooms < 1) {
+cout << "Invalid input. Please enter a value greater than or equal to 1.\n";
+}
+} while (numRooms < 1);
+return numRooms;
+}
 
-double fahrenheitToCelsius(double fahrenheit) {
-return (5.0 / 9.0) * (fahrenheit - 32);
+double validatePaintPrice() {
+double paintPrice;
+do {
+cout << "Enter the price of paint per gallon: $";
+cin >> paintPrice;
+if (paintPrice < 10.00) {
+cout << "Invalid input. Please enter a value greater than or equal to $10.00.\n";
+}
+} while (paintPrice < 10.00);
+return paintPrice;
+}
+
+double validateSquareFootage() {
+double squareFootage;
+do {
+cout << "Enter the square footage of wall space: ";
+cin >> squareFootage;
+if (squareFootage < 0) {
+cout << "Invalid input. Please enter a non-negative value.\n";
+}
+} while (squareFootage < 0);
+return squareFootage;
 }
 
 int main() {
-cout << "Fahrenheit\tCelsius" << endl;
-cout << "-------------------" << endl;
+int numRooms = validateRooms();
 
-for (int fahrenheit = 0; fahrenheit <= 20; fahrenheit++) {
-double celsius = fahrenheitToCelsius(fahrenheit);
-cout << fahrenheit << "\t" << fixed << setprecision(2) << celsius << endl;
+double paintPrice = validatePaintPrice();
+
+double totalSquareFootage = 0;
+for (int i = 1; i <= numRooms; i++) {
+cout << "Room " << i << ":" << endl;
+totalSquareFootage += validateSquareFootage();
 }
+
+double gallonsNeeded = totalSquareFootage / 110.0;
+
+double laborHours = totalSquareFootage / 110.0 * 8.0;
+  
+double paintCost = gallonsNeeded * paintPrice;
+  
+double laborCost = laborHours * 25.00;
+
+double totalCost = paintCost + laborCost;
+
+
+cout << "\nPainting Job Summary:\n";
+cout << "Number of gallons of paint required: " << fixed << setprecision(2) << gallonsNeeded << endl;
+cout << "Hours of labor required: " << fixed << setprecision(2) << laborHours << endl;
+cout << "Cost of paint: $" << fixed << setprecision(2) << paintCost << endl;
+cout << "Labor charges: $" << fixed << setprecision(2) << laborCost << endl;
+cout << "Total cost of paint job: $" << fixed << setprecision(2) << totalCost << endl;
 
 return 0;
 }
